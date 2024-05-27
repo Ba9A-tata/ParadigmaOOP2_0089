9436d18976cf9ecd50b157ff66b2c0d62e31e6e4
Ba9A-tata <yogadzakkii@gmail.com>
2024-05-27 15:03:08 +0700

    membuat class pekerja pada multi waris

--- MULTIWARIS.cpp
+++ MULTIWARIS.cpp
@@ -3,6 +3,16 @@ using namespace std;
 
 class orang {
 public:
-    
+    int umur;
+
+    orang(int pUmur) :
+        umur(pUmur)
+    {
+        cout << "Orang dibuat dengan umur " << umur << "\n" << endl;
+    }
+
+};
+
+class pekerja : virtual public orang {
 
 };
